82844bd63b8f8c9e850ad4ac3284948dd6fe3deb
CloudMagician <luzixv1998@icloud.com>
2019-05-11 15:31:01 +0800

    UITextView Done

--- SNL-Complier/SNL-Complier/ViewController.swift
+++ SNL-Complier/SNL-Complier/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ScrollView: UIScrollView!
+    @IBOutlet weak var TextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        TextView.sizeThatFits(CGSize.init(width: TextView.frame.size.width, height: TextView.frame.size.height))
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.ScrollView.layoutIfNeeded()
+        self.ScrollView.contentSize = self.TextView.bounds.size
+    }
 }
 
