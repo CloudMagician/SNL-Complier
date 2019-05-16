a114e4a18e8be0800b3d2ea806c1026daafd33d2
CloudMagician <luzixv1998@icloud.com>
2019-05-16 10:27:17 +0800

    predictSet

--- SNL-Complier/SNL-Complier/ViewController.swift
+++ SNL-Complier/SNL-Complier/ViewController.swift
@@ -10,43 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var introductionText = """
-    SNL(Small Nested Language)语言是我们自行定义的教学模型语言，它是一种类PASCAL的“高级”程序设计语言。
-    SNL语言的数据结构比较丰富，除了整型、字符型等简单数据类型外，还有数组、记录等结构数据类型，过程允许嵌套定义，允许递归调用。
-    SNL语言基本上包含了高级程序设计语言的所有常用的成分，具备了高级程序设计语言的基本特征，实现SNL的编译器，
-    可以涉及到绝大多数编译技术。通过对SNL语言编译程序的学习，我们可以更加深入更加全面的掌握编译程序的构造原理。
-    但为了教学方便起见，略去了高级程序设计语言的一些复杂成分，如文件、集合、指针的操作等。
-
-
-    SNL编译系统的单词符号分类如下：
-    - 标识符：\t\t\t( ID )
-    - 保留字：\t\t\t(它是标识符的子集, if,repeat,read,write，…)
-    - 无符号整数：\t\t( INTC )
-    - 单字符分界符：\t\t( + , - , * , / , < ,= ,( , ) , [ , ] , . , ; , EOF ,空白字符 )
-    - 双字符分界符：\t\t( := )
-    - 注释头符：\t\t\t( { )
-    - 注释结束符：\t\t( } )
-    - 字符起始和结束符：\t( ‘ )
-    - 数组下标界限符：\t( .. )
-
-
-    语法的非形式说明：
-    一个SNL程序是由程序头、声明部分和程序体组成的。
-    声明部分包括类型声明、变量声明和过程声明。
-    SNL语言的语法规定可以声明整型（integer）、字符类型（char）、数组类型以及记录类型的类型标识符和变量。
-    过程声明包括过程头、过程内部声明和过程体部分，过程声明内部还可以嵌套声明内层过程。
-    程序体由语句序列构成，语句包括空语句、赋值语句、条件语句、循环语句、输入输出语句、过程调用语句和返回语句。
-    表达式分为简单算术表达式和关系表达式。
-    
-    1．程序头的形式是：\t\t关键字program 后面跟着程序名标识符；
-    2．类型定义的形式是：\t类型名标识符=类型定义，其中类型定义可以是类型名或者是结构类型定义，类型名可以是基本类型，
-    \t\t\t\t\t\t或者是前面已经定义的一个类型标识符；
-    3．变量声明的形式是：\t类型名后面跟着用逗号隔开的变量标识符序列；
-    4．过程声明的形式是：\t关键字procedure跟着过程名标识符，以及参数声明、类型定义、变量说明、内层过程声明和程序体；
-    5．程序体的形式是：\t\t以关键字begin开头，关键字end结尾，中间是用分号隔开的语句序列（注意最后一条语句后不加分号），
-    \t\t\t\t\t\t最后用“.”标志程序体的结束。
-    """
+    var introductionText = ""
     var codes = ""
+    var grammarRules = ""
     var tempText = ""
     var Tokens = [Token]()
     
@@ -121,6 +87,136 @@ class ViewController: UIViewController {
     }
     
     @IBAction func Button4(_ sender: UIButton) {
+        grammarRules = """
+        Program         ::= ProgramHead DeclarePart ProgramBody .
+        
+        ProgramHead     ::= PROGRAM ProgramName
+        ProgramName     ::= ID
+
+        DeclarePart     ::= TypeDecpart VarDecpart ProcDecpart
+        
+        TypeDecpart     ::= ε
+                        |   TypeDec
+        TypeDec         ::= TYPE TypeDecList
+        TypeDecList     ::= TypeId = TypeDef ; TypeDecMore
+        TypeDecMore     ::= ε
+                        |   TypeDecList
+        TypeId          ::= ID
+        
+        TypeDef         ::= BaseType
+                        |   StructureType
+                        |   ID
+        BaseType        ::= INTEGER
+                        |   CHAR
+        StructureType   ::= ArrayType
+                        |   RecType
+        ArrayType       ::= ARRAY [ low .. top ] OF BaseType
+        Low             ::= INTC
+        Top             ::= INTC
+        RecType         ::= RECORD FieldDecList END
+        FieldDecList    ::= BaseType IdList ; FieldDecMore
+                        |   ArrayType IdList ; FieldDecMore
+        FieldDecMore    ::= ε
+                        |   FieldDecList
+        IdList          ::= ID IdMore
+        IdMore          ::= ε
+                        |   , IdList
+
+        VarDecpart      ::= ε
+                        |   VarDec
+        VarDec          ::= VAR VarDecList
+        VarDecList      ::= TypeDef VarIdList ; VarDecMore
+        VarDecMore      ::= ε
+                        |   VarDecList
+        VarIdList       ::= ID VarIdMore
+        VarIdMore       ::= ε
+                        |   , VarIdList
+        
+        ProcDecpart     ::= ε
+                        |   ProcDec
+        ProcDec         ::= PROCEDURE ProcName ( ParamList ) ; ProcDecPart ProcBody ProcDecMore
+        ProcDecMore     ::= ε
+                        |   ProcDeclaration
+        ProcName        ::= ID
+        
+        ParamList       ::= ε
+                        |   ParamDecList
+        ParamDecList    ::= Param ParamMore
+        ParamMore       ::= ε
+                        |   ; ParamDecList
+        Param           ::= TypeDef FormList
+                        |   VAR TypeDef FormList
+        FormList        ::= ID FidMore
+        FidMore         ::= ε
+                        |   , FormList
+        
+        ProcDecPart     ::= DeclarePart
+        
+        ProcBody        ::= ProgramBody
+        
+        ProgramBody     ::= BEGIN StmList END
+        
+        StmList         ::= Stm StmMore
+        StmMore         ::= ε
+                        |   ; StmList
+        
+        Stm             ::= ConditionalStm
+                        |   LoopStm
+                        |   InputStm
+                        |   OutputStm
+                        |   ReturnStm
+                        |   ID AssCall
+        
+        AssCall         ::= AssignmentRest
+                        |   CallStmRest
+        
+        AssignmentRest  ::= VariMore := Exp
+        
+        ConditionalStm  ::= IF RelExp THEN StmList ELSE StmList FI
+        LoopStm         ::= WHILE RelExp DO StmList ENDWH
+        InputStm        ::= READ ( Invar )
+        Invar           ::= ID
+        OutputStm       ::= WRITE ( Exp )
+        ReturnStm       ::= RETURN
+        
+        CallStmRest     ::= ( ActParamList )
+        ActParamList    ::= ε
+                        |   Exp ActParamMore
+        ActParamMore    ::= ε
+                        |   , ActParamList
+        
+        RelExp          ::= Exp OtherRelE
+        OtherRelE       ::= CmpOp Exp
+        
+        Exp             ::= Term OtherTerm
+        OtherTerm       ::= ε
+                        |   AddOp Exp
+        
+        Term            ::= Factor OtherFactor
+        OtherFactor     ::= ε
+                        |   MultOp Term
+        
+        Factor          ::= ( Exp )
+                        |   INTC
+                        |   Variable
+        Variable        ::= ID VariMore
+        VariMore        ::= ε
+                        |   [ Exp ]
+                        |   . FieldVar
+        FieldVar        ::= ID FieldVarMore
+        FieldVarMore    ::= ε
+                        |   [ Exp ]
+        
+        CmpOp           ::= <
+                        |   =
+        
+        AddOp           ::= +
+                        |   -
+        
+        MultOp          ::= *
+                        |   /
+        """
+        TextView.text = grammarRules
     }
     
     @IBAction func Button5(_ sender: UIButton) {
@@ -139,6 +235,42 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         TextView.sizeThatFits(CGSize.init(width: TextView.frame.size.width, height: TextView.frame.size.height))
+        introductionText = """
+        SNL(Small Nested Language)语言是我们自行定义的教学模型语言，它是一种类PASCAL的“高级”程序设计语言。
+        SNL语言的数据结构比较丰富，除了整型、字符型等简单数据类型外，还有数组、记录等结构数据类型，过程允许嵌套定义，允许递归调用。
+        SNL语言基本上包含了高级程序设计语言的所有常用的成分，具备了高级程序设计语言的基本特征，实现SNL的编译器，
+        可以涉及到绝大多数编译技术。通过对SNL语言编译程序的学习，我们可以更加深入更加全面的掌握编译程序的构造原理。
+        但为了教学方便起见，略去了高级程序设计语言的一些复杂成分，如文件、集合、指针的操作等。
+        
+        
+        SNL编译系统的单词符号分类如下：
+        - 标识符：\t\t\t( ID )
+        - 保留字：\t\t\t(它是标识符的子集, if,repeat,read,write，…)
+        - 无符号整数：\t\t( INTC )
+        - 单字符分界符：\t\t( + , - , * , / , < ,= ,( , ) , [ , ] , . , ; , EOF ,空白字符 )
+        - 双字符分界符：\t\t( := )
+        - 注释头符：\t\t\t( { )
+        - 注释结束符：\t\t( } )
+        - 字符起始和结束符：\t( ‘ )
+        - 数组下标界限符：\t( .. )
+        
+        
+        语法的非形式说明：
+        一个SNL程序是由程序头、声明部分和程序体组成的。
+        声明部分包括类型声明、变量声明和过程声明。
+        SNL语言的语法规定可以声明整型（integer）、字符类型（char）、数组类型以及记录类型的类型标识符和变量。
+        过程声明包括过程头、过程内部声明和过程体部分，过程声明内部还可以嵌套声明内层过程。
+        程序体由语句序列构成，语句包括空语句、赋值语句、条件语句、循环语句、输入输出语句、过程调用语句和返回语句。
+        表达式分为简单算术表达式和关系表达式。
+        
+        1．程序头的形式是：\t\t关键字program 后面跟着程序名标识符；
+        2．类型定义的形式是：\t类型名标识符=类型定义，其中类型定义可以是类型名或者是结构类型定义，类型名可以是基本类型，
+        \t\t\t\t\t\t或者是前面已经定义的一个类型标识符；
+        3．变量声明的形式是：\t类型名后面跟着用逗号隔开的变量标识符序列；
+        4．过程声明的形式是：\t关键字procedure跟着过程名标识符，以及参数声明、类型定义、变量说明、内层过程声明和程序体；
+        5．程序体的形式是：\t\t以关键字begin开头，关键字end结尾，中间是用分号隔开的语句序列（注意最后一条语句后不加分号），
+        \t\t\t\t\t\t最后用“.”标志程序体的结束。
+        """
         TextView.text = introductionText
     }
     
